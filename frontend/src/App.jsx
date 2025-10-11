7baeda506034cd0c38c65321cae0354da2a609ce
Bishal Bhandari <bhandarisuraksha617@gmail.com>
2025-10-11 10:15:49 +0545

    fix: Change Router from BrowserRouter to HashRouter for improved routing compatibility

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+
 import { AuthProvider } from "./contexts/AuthContext";
 
 import LandingPage from "./pages/LandingPage";
